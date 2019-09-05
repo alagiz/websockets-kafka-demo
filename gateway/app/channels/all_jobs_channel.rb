a681970666cc5609f2133c396283dd27464ba3ed
ArtemAlagizov <alagizov@gmail.com>
2019-09-05 11:25:54 +0200

    extended kafka based communication to handle all jobs request

--- gateway/app/channels/all_jobs_channel.rb
+++ gateway/app/channels/all_jobs_channel.rb
@@ -3,6 +3,10 @@ class AllJobsChannel < ApplicationCable::Channel
     stream_from "all_jobs_channel"
   end
 
+  def receive(message)
+    AllJobsRequestResponder.call({:userId =>  message['userId']}.to_json)
+  end
+
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
